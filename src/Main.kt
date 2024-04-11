64a53697c0cf856c069851c080e05f0cb9970b61
Jhonier Mosquera <jhoniermosquera61@gmail.com>
2024-04-11 17:57:52 -0500

    result

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
+class Song(var title: String, var artist: String, var year: Int)
 fun main() {
-    println("Hello World")
+    val cancion = Song("moon",  "Jhonier", 2024)
+    println("${cancion.title}, interpretada por ${cancion.artist}, se lanzó en ${cancion.year}.")
 }
